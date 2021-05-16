8972d55b13172ce68c8ffaa274ca1525fa1630dd
sugyanmishra29 <sugyanmishra.sinu@gmail.com>
2021-05-16 20:31:13 +0530

    third commit

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # BigText
+
+## Key Features
+1. Text is displayed like a threaded conversation. Latest entered text gets shown at the bottom of the conversation.
+2. When the user closes and reopens the app, previous data is shown. This showing of previous data is handled in the background (as an Asynchronous task) so as to avoid any UI lag while using the app.
+3. Empty messages cannot be sent. The app prompts the user to provide a text input.
+4. Increment in text-size occurs upon long-pressing of the Display button, but this feature is buggy and needs to be fixed. 
+5. User gets a VIBRATION HAPTIC FEEDBACK when the latest entered text appears in the conversation.
+
+## Known Bugs (yet to be fixed)
+1. If the user press and hold the display button, then entered text IN THE CURRENT MESSAGE should keep on increasing in size. However due to slow storing and fetching in SQLite database, the entered text IN THE SUBSEQUENT MESSAGE gets increased.
+2. Text-sizes and increments in them have been hardcoded. 
+3. Other minor bugs (unnoticed)
+
+## Features yet to be implemented
+1. Increment in text size needs to be shown using animations like Bigmoji feature. For the time being, vibration feedback has been implemented which gets triggered when there is an increment in text-size.
+2. Further refinements in UI
--- app/src/main/java/com/example/bigtext/MainActivity.java
+++ app/src/main/java/com/example/bigtext/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.bigtext;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -7,11 +8,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 import android.annotation.SuppressLint;
+import android.app.Service;
 import android.content.Context;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.os.Handler;
+import android.os.IBinder;
 import android.os.VibrationEffect;
 import android.os.Vibrator;
 import android.util.Log;
@@ -20,6 +24,7 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Chronometer;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -33,6 +38,10 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerViewAdapter recyclerViewAdapter;
     private ArrayList<Convo> convoArrayList;
     private ArrayAdapter<String> arrayAdapter;
+    // private boolean flag = false;
+    private int ticks = 0;
+    private static int txtSize = 16; //default text size
+    Convo convo = new Convo();
 
     public class BGprevDataLoader extends AsyncTask<Void, Void, Void>{
         @Override
@@ -65,26 +74,6 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-//    public class BGrecyclerViewUpdation extends AsyncTask<Void, Void, Void>{
-//        @Override
-//        protected void onPreExecute() {
-//            super.onPreExecute();
-//        }
-//
-//        @Override
-//        protected Void doInBackground(Void... voids) {
-//            recyclerViewAdapter = new RecyclerViewAdapter(MainActivity.this, convoArrayList);
-//            recyclerView.setAdapter(recyclerViewAdapter);
-//            recyclerView.scrollToPosition(convoArrayList.size()-1);
-//            return null;
-//        }
-//
-//        @Override
-//        protected void onPostExecute(Void unused) {
-//            super.onPostExecute(unused);
-//        }
-//
-//    }
 
     @SuppressLint("ClickableViewAccessibility")
     @Override
@@ -103,60 +92,26 @@ public class MainActivity extends AppCompatActivity {
         EditText editText = findViewById(R.id.editText);
         Button button = findViewById(R.id.button);
 
-//        button.setOnClickListener(new View.OnClickListener(){
-//            @Override
-//            public void onClick(View v) {
-//                Convo convo = new Convo();
-//                String item = editText.getText().toString();
-//                if (item.equals("")) {
-//                    Toast.makeText(MainActivity.this, "Please provide an input!", Toast.LENGTH_SHORT).show();
-//                } else {
-//                    convo.setConvoText(item);
-//                    db.addConvo(convo);
-//
-//                    convoArrayList = new ArrayList<>();
-//
-//                    List<Convo> convoList = db.getAllConvos();
-//
-//                    for (Convo convos : convoList) {
-//
-//                        Log.d("dbharry", "\nId: " + convos.getId() + "\n" +
-//                                "Convo Text: " + convos.getConvoText() + "\n" +
-//                                "Convo Size: " + convos.getConvoSize() + "\n");
-//
-//                        convoArrayList.add(convos);
-//                    }
-//
-//                    // Use your recyclerView
-//                    recyclerViewAdapter = new RecyclerViewAdapter(MainActivity.this, convoArrayList);
-//                    recyclerView.setAdapter(recyclerViewAdapter);
-//                    recyclerView.scrollToPosition(convoArrayList.size()-1);
-//                    editText.setText("");  //after sending msg, msg box should become blank
-//
-//                    Log.d("dbharry", "Bro you have " + db.getCount() + " conversations in your database");
-//                }
-//            }
-//        })
 
         button.setOnTouchListener(new View.OnTouchListener() {
-            int txtSize = 16; //default text size
-
-            CountDownTimer countDownTimer = new CountDownTimer(10000,100) {
+            CountDownTimer countDownTimer = new CountDownTimer(3000,1000) {
                 @Override
                 public void onTick(long millisUntilFinished) {
                     // after each tick
                     // text size increase and reflect in recyclerView
-                    Vibrator vb = (Vibrator)   getSystemService(Context.VIBRATOR_SERVICE);
-                    vb.vibrate(20);
-                    txtSize = 24;
-                    Log.d("ticking", "size+8 done");
+                    ticks++;
+                    Vibrator vb = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+                    vb.vibrate(40);
+//                    Toast.makeText(MainActivity.this, "Ticks = " + ticks, Toast.LENGTH_SHORT).show();
+                    txtSize += 8*ticks;
+                    convo.setConvoSize(txtSize);
                 }
 
                 @Override
                 public void onFinish() {
                     // after countdown is over, textSize = max and reflect in recyclerView
                     Log.d("ticked", "countdown finished");
-//                    if (txtSize!=12) txtSize=12;
+
                 }
             };
 
@@ -164,21 +119,27 @@ public class MainActivity extends AppCompatActivity {
             public boolean onTouch(View v, MotionEvent event) {
                 if (event.getAction() == MotionEvent.ACTION_DOWN) {
                     // start your timer
-                    //  txtSize++;
-                    Convo convo = new Convo();
                     String item = editText.getText().toString();
 
                     if (item.equals("")) {
-                        Vibrator vb = (Vibrator)   getSystemService(Context.VIBRATOR_SERVICE);
+                        Vibrator vb = (Vibrator)getSystemService(Context.VIBRATOR_SERVICE);
                         vb.vibrate(500);
                         Toast.makeText(MainActivity.this, "Please provide an input!", Toast.LENGTH_SHORT).show();
+
                     } else {
+
                         convo.setConvoText(item);
                         countDownTimer.start();
-                        convo.setConvoSize(txtSize);
+
+                        Log.v("ticking", "size raised by 8 and ticks = " + ticks + " and txtSize = " + txtSize);
+//                        Toast.makeText(MainActivity.this, "countdown started & ticks is " + ticks + " & textsize is " + txtSize, Toast.LENGTH_SHORT).show();
+
+//                        Log.v("txtSize", "txtSize is " + txtSize);
+//                        convo.setConvoSize(txtSize);
+
+                        convoArrayList = new ArrayList<>();
 
                         db.addConvo(convo);
-                        convoArrayList = new ArrayList<>();
                         List<Convo> convoList = db.getAllConvos();
 
                         for (Convo convos : convoList) {
@@ -189,36 +150,24 @@ public class MainActivity extends AppCompatActivity {
                         }
                     }
 
-//                    BGrecyclerViewUpdation myTask2 = new BGrecyclerViewUpdation();
-//                    myTask2.execute();
-
-                    // delay of 500ms before the size increase code
-                    // 2 simultaneous processes - size increase due to button hold AND
-                    // textsize increment in textView being reflected in recyclerView
-
-//                    Handler h =new Handler();
-//
-//                    // 500 ms is the delayed time before execute the code inside the function
-//                    // code inside function - to start setting new text size after the button is held for 500ms
-//                    h.postDelayed(new Runnable() {
-//                        public void run() {
-//                            //put your code here
-//
-//                        }
-//
-//                    }, 500);
-
                 } else if (event.getAction() == MotionEvent.ACTION_UP) {
                     // stop your timer.
                     // Use your recyclerView
+                    Vibrator vb = (Vibrator)getSystemService(Context.VIBRATOR_SERVICE);
+                    vb.vibrate(500);
+//                    Toast.makeText(MainActivity.this, "Ending Countdown", Toast.LENGTH_SHORT).show();
                     countDownTimer.cancel();
                     recyclerViewAdapter = new RecyclerViewAdapter(MainActivity.this, convoArrayList);
                     recyclerView.setAdapter(recyclerViewAdapter);
                     recyclerView.scrollToPosition(convoArrayList.size()-1);
+                    Log.v("ticks", "ticks = " + ticks);
                     editText.setText("");  //after sending msg, msg box should become blank
+                    txtSize = 16;
+                    ticks=0;
                     Log.d("dbSugyan", "Bro you have " + db.getCount() + " conversations in your database");
 //                    countDownTimer.onFinish();
                     db.close();
+
                 }
                 return false;
             }
@@ -226,3 +175,4 @@ public class MainActivity extends AppCompatActivity {
 
     }
 }
+// service class = press service chalu. action up service stop
